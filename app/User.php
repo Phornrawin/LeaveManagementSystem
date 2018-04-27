6e4924c48955bca918cb5f9dbd03d6009d9788e4
jsice <w14chin@gmail.com>
2018-04-27 13:39:25 +0700

    create models and migrations

--- app/User.php
+++ app/User.php
@@ -15,7 +15,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 
+        'firstname', 'lastname', 'gender', 'image', 
+        'supervisor_id', 'department_id', 'position',
+        'tel', 'facebook', 'line',
     ];
 
     /**
