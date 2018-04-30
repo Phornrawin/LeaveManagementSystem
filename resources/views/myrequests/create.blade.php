c0940595efb8aaebcb64fe8023ff3c2c7a7ebe4b
Jutinapas <jutinapas@gmail.com>
2018-05-01 00:59:08 +0700

    update view for make new leave request

--- resources/views/myrequests/create.blade.php
+++ resources/views/myrequests/create.blade.php
@@ -9,7 +9,78 @@
   <h3 class="display-4">Make new leave request</h3>
   <hr>
   <div class="container">
-    <form action="#" method="post">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
+    <form action="/myrequests/create" method="post">
+      @csrf
+      <div class="row">
+        <div class="col-sm-3" id="div-category">
+          <label for="select-category">Select Category</label>
+          <select name="category_id" class="form-control" id="select-category">
+            <option value="" selected disabled hidden>Choose here</option>
+            @foreach($categories as $category)
+            @if(old('category') == $category->name)
+            <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+            @else
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endif
+            @endforeach
+          </select>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col-sm-3" id="div-substitute">
+          <label for="select-substitute">Select Substitute</label>
+          <select name="substitute_id" class="form-control" id="select-substitute">
+            <option value="" selected disabled hidden>Choose here</option>
+            @foreach($users as $user)
+            @if(old('user') == $user->fullName)
+            <option value="{{ $user->id }}" selected>{{ $user->fullName }}</option>
+            @else
+            <option value="{{ $user->id }}">{{ $user->fullName }}</option>
+            @endif
+            @endforeach
+          </select>
+        </div>
+        <div class="col-sm-3" id="div-task">
+          <label for="select-task">Select Task</label>
+          <select name="task_id" class="form-control" id="select-task">
+            <option value="" selected disabled hidden>Choose here</option>
+            @foreach($tasks as $task)
+            @if(old('task') == $task->name)
+            <option value="{{ $task->id }}" selected>{{ $task->name }}</option>
+            @else
+            <option value="{{ $task->id }}">{{ $task->name }}</option>
+            @endif
+            @endforeach
+          </select>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col-sm-3">
+          <label for="input-start">Start Date</label>
+          <input name="start_date" class="form-control" type="date" id="input-start">
+          <script>
+            document.querySelector("#input-start").valueAsDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+          </script>
+        </div>
+        <div class="col-sm-3">
+          <label for="input-start">End Date</label>
+          <input name="end_date" class="form-control" type="date" id="input-end">
+        </div>
+        <div class="col-sm-3">
+          <button type="submit" class="btn btn-primary m-3" style="font-size: 26px"><i class="far fa-plus-square"></i>&nbsp;SUBMIT</button>
+        </div>
+      </div>
     </form>
   </div>
 </div>
