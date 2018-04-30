d84240e409731c8283aa350bbe2090a3abdfae6e
Jutinapas <jutinapas@gmail.com>
2018-05-01 01:12:42 +0700

    add href in sidebar to make new leave

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -8,7 +8,7 @@
                 <a id="collapseLeaveBtn" class="list-group-item list-group-item-action collapsed" data-toggle="collapse" data-target="#collapseLeave" aria-expanded="false" aria-controls="collapseLeave">My Leaves  <i class="fas fa-caret-down"></i><span class="badge badge-pill badge-success float-right"> 1 </span></a>
                 <div id="collapseLeave" class="collapse" aria-labelledby="#collapseLeaveBtn">
                     <a href="/myrequests" class="list-group-item list-group-item-action bg-light">Show all my leaves</a>
-                    <a class="list-group-item list-group-item-action bg-light">Make new leave request</a>
+                    <a href="/myrequests/create" class="list-group-item list-group-item-action bg-light">Make new leave request</a>
                 </div>
                 <a class="list-group-item list-group-item-action collapsed" data-toggle="collapse" data-target="#collapseRequests" aria-expanded="false" aria-controls="collapseRequests">Requests to me <i class="fas fa-caret-down"></i><span class="badge badge-pill badge-success float-right"> 3 </span></a>
                 <div id="collapseRequests" class="collapse">
